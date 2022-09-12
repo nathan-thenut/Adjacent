919ae5dd47d59f18169b45fd22eea11e452d9b84
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2022-09-12 22:25:21 +0200

    added test file and cmake option

---
+++ test.py
@@ -0,0 +1,65 @@
+from build.adjacent_api import *
+import math
+
+# Adjacent computes everything symbolically so we need to create named parameters
+
+x = Param("x", 0.0)
+y = Param("y", 5.0)
+z = Param("z", 1.0)
+
+# Now we can create a point at (0, 5, 1)
+p1 = Point(x, y, z)
+
+
+# helper function
+def point(name, xyz=(0, 0, 0)):
+    if len(xyz) <= 2:
+        xyz = (*xyz, 0)
+    return Point(Param(f"{name}_x", xyz[0]), Param(f"{name}_y", xyz[1]),
+                 Param(f"{name}_z", xyz[2]))
+
+
+p2 = point("p2", (3, 1, 2))
+p3 = point("p3", (5, 5, 9))
+p4 = point("p4", (1, 2, 9))
+p5 = point("cc1", (1, 1))
+
+l1 = Line(p1, p2)
+l2 = Line(p3, p4)
+c1 = Circle(p5, Param("rad1", 1.2))
+
+s = Sketch()
+
+s.add_entity(l1)
+s.add_entity(l2)
+s.add_entity(c1)
+
+DESIRED_ANGLE = -0.8 * math.pi
+
+# Let's add some constraints to the sketch
+s.add_constraint(constraints.Coincident(p2, p4))
+s.add_constraint(constraints.Diameter(c1, 2.5))
+
+s.add_constraint(constraints.Coincident(p1, p5))
+# s.add_constraint(constraints.Tangent(c1, l1))
+
+s.add_constraint(constraints.Length(l1, 5))
+s.add_constraint(constraints.Length(l2, 1.5))
+
+# Horizontal / Vertical constraint
+s.add_constraint(constraints.HV(l1, constraints.HVOrientation.OY))
+
+# s.add_constraint(constraints.HV(l2, constraints.HVOrientation.OY))
+s.add_constraint(constraints.Angle(l1, l2, DESIRED_ANGLE))
+# s.add_constraint(constraints.Parallel(l1, l2))
+# s.add_constraint(constraints.HV(l1, constraints.HVOrientation.OX))
+# s.add_constraint(constraints.Length(l2, 1.5))
+# s.add_constraint(constraints.Distance(p2, p3, 0.5))
+# s.add_constraint(constraints.PointOn(p3, l1))
+
+# And solve!
+s.update()
+
+print("L1: ", l1)
+print("L2: ", l2)
+print("C1: ", c1)
