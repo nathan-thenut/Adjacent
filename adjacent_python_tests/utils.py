d791e4c9e34737e7e14e4c1ad7510a3be1ae55cc
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2022-10-26 15:18:15 +0200

    added methods for calculating sparsity & changes

--- adjacent_python_tests/utils.py
+++ adjacent_python_tests/utils.py
@@ -87,11 +87,31 @@ def export_entities_to_dict(
 def add_comparison_data(data: dict[str, dict]) -> dict[str, dict]:
     """Add data to compare the different solutions."""
     new_data = data
+    variables = 0
+    sum_of_changes = {}
+    non_zero_results = {}
+    for result in [Result.L1.name, Result.L2.name]:
+        sum_of_changes[result] = 0
+        non_zero_results[result] = 0
+
     if "lines" in new_data.keys():
         for line in new_data["lines"].keys():
             points = new_data["lines"][line]["points"]
             for pnt in points.keys():
+                variables += 2
                 add_comparison_data_to_point(points[pnt])
+                for result in [Result.L1.name, Result.L2.name]:
+                    if result in points[pnt].keys():
+                        sum_of_changes[result] += points[pnt][result][
+                            "sum_of_changes"]
+                        non_zero_results[result] += points[pnt][result][
+                            "non_zero_results"]
+
+    results = {}
+    results["sum_of_changes"] = sum_of_changes
+    results["non_zero_results"] = non_zero_results
+    results["variables"] = variables
+    new_data["Results"] = results
     return new_data
 
 
@@ -102,18 +122,35 @@ def add_comparison_data_to_point(point_data: dict[str, dict]):
         if result in point_data.keys():
             soc = calculate_sum_of_changes(original_data, point_data[result])
             point_data[result]["sum_of_changes"] = soc
+            non_zero_results = count_non_zeroes_in_result(
+                original_data, point_data[result])
+            point_data[result]["non_zero_results"] = non_zero_results
 
 
 def calculate_sum_of_changes(
     original: dict[str, float],
     new: dict[str, float],
 ) -> float:
-    """Calculate the sum of changes between original and new data."""
+    """Calculate the sum of changes between original and new point data."""
     return np.sum(
         np.abs([
             original["x"] - new["x"],
             original["y"] - new["y"],
         ]))
+
+
+def count_non_zeroes_in_result(
+    original: dict[str, float],
+    new: dict[str, float],
+) -> int:
+    """Count non zero results in points for sparsity calculation."""
+    non_zero_results = 0
+    for var in original.keys():
+        diff = original[var] - new[var]
+        if diff != 0:
+            non_zero_results += 1
+
+    return non_zero_results
 
 
 def write_data_to_json_file(path: Path,
