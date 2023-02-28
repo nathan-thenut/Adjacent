4d4122421339dace061f02c74cb10b3cdce51c0d
Nathan Eikermann <tensazangetsu1990@googlemail.com>
2023-02-28 23:32:55 +0100

    added new pentagon 01 data for angle 01

--- adjacent_python_tests/pentagon_01.py
+++ adjacent_python_tests/pentagon_01.py
@@ -4,7 +4,7 @@ import numpy as np
 from core_utils import (PyConstraints, create_and_solve_sketch,
                         generate_vectors_with_distance)
 
-offset_pairs = generate_vectors_with_distance(1, count=7)
+offset_pairs = generate_vectors_with_distance(1, count=1000)
 for i in range(len(offset_pairs)):
     points = {}
     points["p1"] = (0.6156184929834163, 0.08539832151226268)
@@ -43,23 +43,23 @@ for i in range(len(offset_pairs)):
         "value": -ANGLE
     }
 
-    constraint_dict["c3"] = {
-        "type": PyConstraints.ANGLE,
-        "entities": ["l3", "l4"],
-        "value": -ANGLE
-    }
+    # constraint_dict["c3"] = {
+    #     "type": PyConstraints.ANGLE,
+    #     "entities": ["l3", "l4"],
+    #     "value": -ANGLE
+    # }
 
-    constraint_dict["c4"] = {
-        "type": PyConstraints.ANGLE,
-        "entities": ["l4", "l5"],
-        "value": -ANGLE
-    }
+    # constraint_dict["c4"] = {
+    #     "type": PyConstraints.ANGLE,
+    #     "entities": ["l4", "l5"],
+    #     "value": -ANGLE
+    # }
 
-    constraint_dict["c5"] = {
-        "type": PyConstraints.ANGLE,
-        "entities": ["l5", "l1"],
-        "value": -ANGLE
-    }
+    # constraint_dict["c5"] = {
+    #     "type": PyConstraints.ANGLE,
+    #     "entities": ["l5", "l1"],
+    #     "value": -ANGLE
+    # }
 
     # constraint_dict["c6"] = {"type": PyConstraints.EQUAL, "entities": ["l6", "l7"]}
     #
@@ -73,7 +73,7 @@ for i in range(len(offset_pairs)):
     # }
     #
 
-    selected_point = random.choice(list(points.keys())[:2])
+    selected_point = random.choice(list(points.keys())[:4])
     selected_values = points[selected_point]
     xoffset = offset_pairs[i][0]
     yoffset = offset_pairs[i][1]
